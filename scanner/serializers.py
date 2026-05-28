7a5e95eaf7355060bf9b8bdcb0ebcec8e8d7531f
jeetam1 <jjeetamsinghyadav12@gmail.com>
2026-05-28 22:34:14 +0530

    frontend fix

--- scanner/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ScanHistory
-
-class ScanHistorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ScanHistory
-        fields = ['id', 'filename', 'original_text', 'masked_text', 'risk_score', 'risk_level', 'created_at']
